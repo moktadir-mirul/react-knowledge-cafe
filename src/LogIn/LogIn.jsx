404f64a07e89bd36a435b68cf47fa9dfb5e7799b
moktadir-mirul <moktadir.mirul@gmail.com>
2025-04-29 23:26:56 +0600

    sign in create user successful

--- src/LogIn/LogIn.jsx
+++ src/LogIn/LogIn.jsx
@@ -1,25 +1,49 @@
-import React from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
 import { Link } from "react-router";
+import { auth } from "../firebase";
 
 const LogIn = () => {
+    const [user, setUser] = useState(null);
+    const [errorMsg, setErrorMsg] = useState('')
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        setUser(null);
+        setErrorMsg('');
+        let email = e.target.email.value;
+        let password = e.target.password.value;
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+            console.log(result.user)
+            setUser(result.user);
+        })
+        .catch((error) => {
+            console.log(error);
+            setErrorMsg(error.message)
+        })
+    }
   return (
     <>
         <div className="mt-30 card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
         <h1 className="text-5xl font-bold">Login now!</h1>
       <div className="card-body">
-        <fieldset className="fieldset">
+        <form onSubmit={handleLogin} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" className="input" name="email" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name="password" className="input" placeholder="Password" />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
           <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+        </form>
       </div>
     </div>
     <Link to={"/"}>Go back</Link>
+    <h1 className="text-3xl text-green-600">{user && 'User logged in successfully'}</h1>
+    <h1 className="text-3xl text-red-600">{errorMsg && errorMsg}</h1>
     </>
   );
 };
--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,25 +1,93 @@
-import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { IoEye, IoEyeOff } from "react-icons/io5";
 import { Link } from "react-router";
+import { auth } from "../firebase";
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [createUser, setCreatedUser] = useState(null);
+
+  const handleCreateUser = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    setCreatedUser(null);
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result => {
+        console.log(result.user);
+        setCreatedUser(result.user)
+    })
+    .catch(error => {
+        console.log(error.message);
+        setErrorMsg(true);
+    })
+  }
+
   return (
     <div>
-      <div className="mt-30 card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
-        <h1 className="text-5xl font-bold">Please, sign up first!</h1>
-        <div className="card-body">
-          <fieldset className="fieldset">
-            <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
-            <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
-            <div>
-              <a className="link link-hover">Forgot password?</a>
-            </div>
-            <button className="btn btn-neutral mt-4">Login</button>
-          </fieldset>
+      <form onSubmit={handleCreateUser}>
+        <div className="mt-30 card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
+          <h1 className="text-4xl font-bold p-2 text-center">
+            Please, sign up first!
+          </h1>
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">Name</label>
+              <input
+                type="text"
+                className="input"
+                name="name"
+                placeholder="Name"
+              />
+              <label className="label">Photo URL</label>
+              <input
+                type="text"
+                className="input"
+                name="photoURL"
+                placeholder="Photo URL"
+              />
+              <label className="label">Email</label>
+              <input
+                type="email"
+                className="input"
+                placeholder="Email"
+                name="email"
+              />
+              <div className="relative">
+                <label className="label">Password</label>
+                <input
+                  name="password"
+                  type={showPassword ? "text" : "password"}
+                  className="input"
+                  placeholder="Password"
+                />
+                <button
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="btn btn-sm absolute top-[22px] right-6 z-50"
+                >
+                  {showPassword === false ? (
+                    <IoEyeOff size={"25px"}></IoEyeOff>
+                  ) : (
+                    <IoEye size={"25px"}></IoEye>
+                  )}
+                </button>
+              </div>
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </div>
         </div>
-      </div>
+      </form>
       <Link to={"/"}>Go back</Link>
+      <h1 className="text-3xl text-green-600">{createUser && 'User created successfully'}</h1>
+      <h1 className="text-3xl text-red-600">{errorMsg && 'Thers been an error'}</h1>
     </div>
   );
 };
